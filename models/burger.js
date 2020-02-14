11c1c34716ecb4850cb0541a8b18676554535266
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-14 15:19:37 -0500

    created functions that will call the ORM functions using burger specific input for the ORM

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+const burger = {
+    selectAll: (cb) => {
+        orm.selectAll((res) => {
+            cb(res);
+        });
+    },
+    insertOne: (vals, cb) => {
+        orm.insertOne(vals, (res) => {
+            cb(res);
+        });
+    },
+    undateOne: (cb) => {
+        orm.updateOne((res) => {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
