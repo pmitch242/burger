414d080ab84a6a9d8a4eb4a48c45610d406c9a25
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-14 16:25:15 -0500

    added controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,34 @@ const express = require("express");
 const router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-const cat = require("../models/burger");
+const burger = require("../models/burger");
 
+router.get("/", (req, res) => {
+    burger.selectAll((data) => {
+        const burgerObject = {
+            burgers: data
+        };
+        res.json(burgerObject);
+    })
+});
+
+router.post("/api/burger", (req, res) => {
+    burger.insertOne([req.body.burger_name], (result) => {
+        // Send back the ID
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burger/:id", (req, res) => {
+    let condition = "id = " + req.params.id;
+    burger.updateOne(condition, (result) => {
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
