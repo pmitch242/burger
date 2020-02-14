989c7c9894109e4eb725b94cb96da935191df327
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-14 14:20:54 -0500

    Initial commit

---
+++ config/orm.js
@@ -0,0 +1,40 @@
+// Import MySQL connection.
+const connection = require("./connection");
+
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+    const arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    const arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (let key in ob) {
+        let value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+const orm = {};
+
+module.exports = orm;
