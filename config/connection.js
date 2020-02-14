93896ca6017039626b00117f3cb38539b1043d3b
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-14 15:42:22 -0500

    changed database name

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "burger_db"
+    database: "burgers_db"
 });
 
 // Make connection.
