1ef813704394c7520fbe7c6d842a5ff6c054ea6d
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-30 22:32:03 -0400

    JawsDB configuration

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+var connection;
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
 // Make connection.
 connection.connect(function (err) {
     if (err) {
